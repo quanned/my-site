fe2c90fddb401c46562c1fc31c32b4717cd34bbb
quanned <avdeyartem1@gmail.com>
2020-07-16 11:40:22 +0300

    add cursor animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-    var nice = $("body").niceScroll();
-  });
+$(document).ready(function () {
+  var nice = $("body").niceScroll();
+});
+
+var cursor = document.querySelector(".cursor");
+var cursorSmall = document.querySelector(".cursorSmall");
+document.addEventListener("mousemove", function (e) {
+  cursor.style.cssText = cursorSmall.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+});
